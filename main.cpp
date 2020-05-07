e393453f4e58f3c63ed433953cb1357a6c7cbfb3
Eruvarn <jakobalwall@gmail.com>
2020-05-08 00:46:51 +0200

    Gjorde en massa saker.

--- main.cpp
+++ main.cpp
@@ -3,16 +3,27 @@
 #include <unistd.h>
 #include <vector>
 #include <math.h>
+#include <map>
+#include <tuple>
+#include "main.hpp"
 
 #define WIDTH 1980
 #define HEIGHT 1080
 
-#define WAIT_TIME 1000 // Microseconds
+#define WAIT_TIME 100 // Microseconds
 
 #define TRAIL_WIDTH 2
 #define TRAIL_DURATION -1
 
+#define GRAVITY_COEFFICIENT 0.00001
+#define LARGE_MASS 10000
+#define DISTANCE 200
+#define INITIAL_SPEED std::sqrt(GRAVITY_COEFFICIENT * LARGE_MASS / DISTANCE)
+
 using std::vector;
+using std::map;
+using std::pair;
+using std::get;
 
 sf::Color BACKGROUND_COLOR = sf::Color(0x39, 0x39, 0x3A);
 sf::Color PLANET_COLOR = sf::Color(0xFF, 0x6B, 0x6B);
@@ -22,181 +33,208 @@ double square(double a) {
   return a * a;
 }
 
-class Object : public sf::Drawable {
-  double _x, _y,
-    _dx, _dy, _fx, _fy, _mass; 
-
-  void move() {
+void Object::move() {
     _x += _dx;
     _y += _dy;
-  }
+}
 
-protected:
-  Object() {
-    
-  }
+Object::Object() {}
   
-  Object(double x, double y, double dx, double dy, double mass) {
+Object::Object(double x, double y, double dx, double dy, double mass) {
     _x = x;
     _y = y;
     _dx = dx;
     _dy = dy;
     _fx = _fy = 0.0;
     _mass = mass;
-  }
+}
 
-public:
+void Object::draw(sf::RenderTarget& t, const Universe* u) const {}
 
-  double x() const {
-    return _x;
-  }
+double Object::x() const {
+  return _x;
+}
 
-  double y() const {
-    return _y;
-  }
 
-  double dx() const {
-    return _dx;
-  }
+double Object::y() const {
+  return _y;
+}
 
-  double dy() const {
-    return _dy;
-  }
+double Object::dx() const {
+  return _dx;
+}
 
-  double mass() const {
-    return _mass;
-  }
+double Object::dy() const {
+  return _dy;
+}
 
-  void applyForce(double fx, double fy) {
-    _fx += fx;
-    _fy += fy;
-  }
+double Object::mass() const {
+  return _mass;
+}
 
-  double distance(Object* o) {
-    return std::sqrt(square(x() - o->x()) + square(y() - o->y()));
-  }
+void Object::applyForce(double fx, double fy) {
+  _fx += fx;
+  _fy += fy;
+}
+
+double Object::distance(Object* o) const {
+  return std::sqrt(square(x() - o->x()) + square(y() - o->y()));
+}
   
-  void update() {
-    move();
-    _dx += _fx / _mass;
-    _dy += _fy / _mass;
-    _fx = 0.0;
-    _fy = 0.0;
-  }
-};
+void Object::update() {
+  move();
+  _dx += _fx / _mass;
+  _dy += _fy / _mass;
+  _fx = 0.0;
+  _fy = 0.0;
+}
 
-class Planet : public Object {
-  double _radius;
 
-public:
-  double radius() const {
-    return _radius;
-  }
+double Planet::radius() const {
+  return _radius;
+}
   
-  Planet(double x, double y, double dx, double dy, double radius, double mass) : Object(x, y, dx, dy, mass) {
-    _radius = radius;
+Planet::Planet(double x, double y, double dx, double dy, double radius, double mass) : Object(x, y, dx, dy, mass) {
+  _radius = radius;
+}
+
+void Planet::draw(sf::RenderTarget& target, const Universe* universe) const {
+  sf::CircleShape shape(radius());
+  shape.setFillColor(PLANET_COLOR);
+  int x0 = x() - universe->xOrigin();
+  int y0 = y() - universe->yOrigin();
+  shape.setPosition(x0 - radius(), y0 - radius());
+  target.draw(shape);
+}
+
+
+Point::Point(int _x, int _y) {
+    x = _x;
+    y = _y;
+}
+
+bool Point::operator>(const Point o) const {
+  if (x == o.x) {
+    return y > o.y;
   }
+  return x > o.x;
+}
 
-  virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {
-    sf::CircleShape shape(radius());
-    shape.setFillColor(PLANET_COLOR);
-    shape.setPosition(x() - radius(), y() - radius());
-    target.draw(shape);
+bool Point::operator<(const Point o) const {
+  if (x == o.x) {
+    return y < o.y;
   }
-};
+  return x < o.x;
+}
+
+bool Point::operator==(const Point o) const {
+  return x == o.x && y == o.y;
+}
 
 
-
-struct VisitInfo {
-  int lastVisited;
-};
-
-class Universe : public sf::Drawable {
-  vector<Object*> objects;
-  vector<VisitInfo> visits;
-  int currentTime;
-
-
-  vector<Object*> initialCondition() {
+vector<Object*> Universe::initialCondition() {
     vector<Object*> objects;
-    objects.push_back(new Planet(550, 550, 0, 0, 50, 10000));
-    objects.push_back(new Planet(550, 700, 1.7, 0, 10, 10000 / 80));
+    objects.push_back(new Planet(550, 550, 0, 0, 10, LARGE_MASS));
+    objects.push_back(new Planet(550, 550 - DISTANCE, INITIAL_SPEED, 0, 5, 0.00001));
+    objects.push_back(new Planet(200, 200, 0.01, 0, 10, 10));
     return objects;
-  }
+}
 
-  void applyGravity() {
-    for (Object* source : objects) {
-      for (Object* target : objects) {
-        if (source == target) {
-          continue;
-        }
-        double forceCoeff = 0.1;
-        double distance = source->distance(target);
-        double forceStrength = forceCoeff * source->mass() * target->mass() / square(distance);
-        double forceX = forceStrength * (source->x() - target->x()) / distance;
-        double forceY = forceStrength * (source->y() - target->y()) / distance;
-        target->applyForce(forceX, forceY);
+void Universe::applyGravity() {
+  for (Object* source : objects) {
+    for (Object* target : objects) {
+      if (source == target) {
+        continue;
+      }
+      double distance = source->distance(target);
+      double forceStrength = GRAVITY_COEFFICIENT * source->mass() * target->mass() / square(distance);
+      double forceX = forceStrength * (source->x() - target->x()) / distance;
+      double forceY = forceStrength * (source->y() - target->y()) / distance;
+      target->applyForce(forceX, forceY);
+    }
+  }
+}
+
+void Universe::resetVisits() {
+  visited = map<Point, int>();
+}
+
+void Universe::freeObjects() {
+  for (Object* object : objects) {
+    free(object);
+  }
+}
+
+void Universe::clearVisited() {
+  if (TRAIL_DURATION != -1) {
+    map<Point, int>::iterator iter = visited.begin();
+    while (iter != visited.end()) {
+      int lastVisit = iter->second;
+      if (currentTime - lastVisit >= TRAIL_DURATION) {
+        iter = visited.erase(iter);
+      } else {
+        ++iter;
       }
     }
   }
-
-  void resetVisits() {
-    for (int i = 0 ; i < WIDTH*HEIGHT ; i++) {
-      visits[i].lastVisited = -1;
-    }    
-  }
-
-  void freeObjects() {
-    for (Object* object : objects) {
-      free(object);
-    }
-  }
+}
   
-public:
-  Universe() {
-    objects = initialCondition();
-    visits.resize(WIDTH * HEIGHT);
-    resetVisits();
-  }
+Universe::Universe() {
+  objects = initialCondition();
+  resetVisits();
+  _x = 0;
+  _y = 0;
+}
 
-  void reset() {
-    freeObjects();
-    resetVisits();
-    objects = initialCondition();
-  }
+double Universe::xOrigin() const {
+  return _x;
+}
 
-  void update() {
-    currentTime++;
-    applyGravity();
-    for (Object* object : objects) {
-      object->update();
-      double x = object->x();
-      double y = object->y();
-      if (x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT) {
-        visits[WIDTH * (int)y + (int)x].lastVisited = currentTime;        
-      }
+double Universe::yOrigin() const {
+  return _y;
+}
+
+void Universe::reset() {
+  freeObjects();
+  resetVisits();
+  objects = initialCondition();
+}
+
+void Universe::update() {
+  currentTime++;
+  applyGravity();
+  for (Object* object : objects) {
+    object->update();
+    double x = object->x();
+    double y = object->y();
+    visited[Point(x, y)] = currentTime;
+  }
+  clearVisited();
+}
+
+void Universe::draw(sf::RenderTarget& target, sf::RenderStates states) const {
+  for (pair<Point, int> element : visited) {
+    int x = get<0>(element).x;
+    int y = get<0>(element).y;
+    int lastVisit = get<1>(element);
+
+    if (TRAIL_DURATION == -1 || (currentTime - lastVisit < TRAIL_DURATION)) {
+      sf::RectangleShape shape(sf::Vector2f(TRAIL_WIDTH,TRAIL_WIDTH));
+      shape.setPosition(x - (int)xOrigin(), y - yOrigin());
+      shape.setFillColor(TRAIL_COLOR);
+      target.draw(shape);        
     }
   }
-
-  virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {
-    for (int x = 0 ; x < WIDTH ; x++) {
-      for (int y = 0 ; y < HEIGHT ; y++) {
-        VisitInfo visit = visits[WIDTH * y + x];
-        if (visit.lastVisited != -1 && (std::abs(currentTime - visit.lastVisited) < TRAIL_DURATION || TRAIL_DURATION == -1)) {
-          sf::RectangleShape shape(sf::Vector2f(TRAIL_WIDTH,TRAIL_WIDTH));
-          shape.setPosition(x, y);
-          shape.setFillColor(TRAIL_COLOR);
-          target.draw(shape);
-        }
-      }
-    }
     
-    for (Object* object : objects) {
-      target.draw(*object);
-    }
+  for (Object* object : objects) {
+    object->draw(target, this);
   }
-  
-};
+}
+
+void Universe::translate(double dx, double dy) {
+  _x += dx;
+  _y += dy;
+}
 
 int main() {
   sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Hello world");
@@ -211,13 +249,28 @@ int main() {
       } else if (event.type == sf::Event::KeyPressed) {
         if (event.key.code == sf::Keyboard::Space) {
           universe.reset();
+        } else if (event.key.code == sf::Keyboard::Q) {
+          window.close();
         }
       }
     }
+    double moveDistance = 0.1;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+      universe.translate(0, -moveDistance);
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+      universe.translate(0, moveDistance);
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+      universe.translate(-moveDistance, 0);
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+      universe.translate(moveDistance, 0);
+    }
+    
     window.clear(BACKGROUND_COLOR);
     universe.update();
     window.draw(universe);
-
 
     window.display();
     usleep(WAIT_TIME);
---
+++ main.hpp
@@ -0,0 +1,74 @@
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <map>
+
+using std::vector;
+using std::map;
+
+class Universe;
+
+class Object {
+  double _x, _y, _dx, _dy, _fx, _fy, _mass;
+
+  void move();
+
+protected:
+  Object();
+
+  Object(double,double,double,double,double);
+
+public:
+  double x() const;
+  double y() const;
+  double dx() const;
+  double dy() const;
+  double mass() const;
+
+  void applyForce(double,double);
+  double distance(Object*) const;
+
+  void update();
+  virtual void draw(sf::RenderTarget&, const Universe*) const;
+};
+
+struct Point {
+  int x, y;
+  Point(int, int);
+  bool operator>(const Point) const;
+  bool operator<(const Point) const;
+  bool operator==(const Point) const;
+};
+
+class Universe : public sf::Drawable {
+  vector<Object*> objects;
+  map<Point, int> visited;
+  int currentTime;
+  double _x, _y;
+
+  vector<Object*> initialCondition();
+  void applyGravity();
+  void resetVisits();
+  void freeObjects();
+  void clearVisited();
+
+public:
+  Universe();
+  double xOrigin() const;
+  double yOrigin() const;
+  void reset();
+  void update();
+  void translate(double, double);
+  virtual void draw(sf::RenderTarget&, sf::RenderStates) const;
+};
+
+class Planet : public Object {
+  double _radius;
+
+public:
+  double radius() const;
+  Planet(double, double, double, double, double, double);
+  void draw(sf::RenderTarget&, const Universe*) const;
+};
+
+
+
